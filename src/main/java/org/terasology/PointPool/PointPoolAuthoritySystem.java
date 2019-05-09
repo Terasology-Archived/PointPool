43a1a67cfb3003a8c6cc64224bdb805628c2b4ab
E Aakash <09e.aakash@gmail.com>
2019-05-09 21:40:29 +0530

    Initital commit

---
+++ src/main/java/org/terasology/PointPool/PointPoolAuthoritySystem.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.PointPool;
+
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.PointPool.event.FillPoolEvent;
+
+@RegisterSystem(RegisterMode.AUTHORITY)
+public class PointPoolAuthoritySystem extends BaseComponentSystem {
+
+    @ReceiveEvent
+    private void fillPool(FillPoolEvent event, PointPoolComponent poolComponent) {
+        poolComponent.poolValue += event.getValue();
+        if (poolComponent.poolValue > poolComponent.maxPoolValue) {
+            poolComponent.poolValue = poolComponent.maxPoolValue;
+        }
+    }
+
+    private void drainPool() {
+    }
+
+    private void instantDrain() {
+    }
+
+    private void instantFill() {
+    }
+
+    private float getStatus() {
+        return 0;
+    }
+
+}
